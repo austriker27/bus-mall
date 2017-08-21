c612cf235fef39801141299fc814923b7c5d2461
austriker27 <davidalindahl@gmail.com>
2017-08-21 13:52:24 -0700

    starting on js file

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// use a constructor with properties for all images
+var widgetGenerator = (properties, name, filepath, timesShown, timesClicked) {
+    this.properties = properties;
+    this.name = name;
+    
+
+
+
+
+
+}
