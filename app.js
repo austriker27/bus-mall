763719d4fd3fc8b338407d8e87e625cecbbe8a46
austriker27 <davidalindahl@gmail.com>
2017-08-21 15:39:56 -0700

    rendering 3 images via JS

--- app.js
+++ app.js
@@ -6,33 +6,34 @@ function widgetImage(name, filepath, timesShown, timesClicked) {
   this.filepath = filepath;
   this.timesShown = timesShown;
   this.timesClicked = timesClicked;
-  // array of all widgetImgs
-  this.allWidgets = [ ];
 
-  // allWidgets.push(this);
+  allWidgets.push(this);
 };
+
+// array of all widgetImgs
+var allWidgets = [];
 
 // all instances of each image
 var bag = new widgetImage('bag', 'img/bag.jpg', 0, 0);
-// var banana = new widgetImage('banana', 'img/banana.jpg', 0, 0);
-// var bathroom = new widgetImage('bathroom', 'img/bathroom.jpg', 0, 0);
-// var boots = new widgetImage('boots', 'img/boots.jpg ', 0, 0);
-// var breakfast = new widgetImage('breakfast', 'img/breakfast.jpg', 0, 0);
-// var bubblegum = new widgetImage('bubblegum', 'img/bubblegum.jpg ', 0, 0);
-// var chair = new widgetImage('chair', 'img/chair.jpg', 0, 0);
-// var cthulhhu = new widgetImage('cthulhhu', 'img/cthulhhu.jpg', 0, 0);
-// var dogDuck = new widgetImage('dog-duck', 'img/dog-duck.jpg ', 0, 0);
-// var dragon = new widgetImage('dragon', 'img/dragon.jpg', 0, 0);
-// var pen = new widgetImage('pen', 'img/pen.jpg', 0, 0);
-// var petSweep = new widgetImage('pet-sweep', 'img/pet-sweep.jpg', 0, 0);
-// var scissors = new widgetImage('scissors', 'img/scissors.jpg', 0, 0);
-// var shark = new widgetImage('shark', 'img/shark.jpg', 0, 0);
-// var sweep = new widgetImage('sweep', 'img/sweep.jpg', 0, 0);
-// var tauntaun = new widgetImage('tauntaun', 'img/tauntaun.jpg', 0, 0);
-// var unicorn = new widgetImage('unicorn', 'img/unicorn.jpg', 0, 0);
-// var usb = new widgetImage('usb', 'img/usb.jpg', 0, 0);
-// var waterCan = new widgetImage('water-can', 'img/water-can.jpg', 0, 0);
-// var wineGlass = new widgetImage('wine-glass', 'img/wine-glass.jpg', 0, 0);
+var banana = new widgetImage('banana', 'img/banana.jpg', 0, 0);
+var bathroom = new widgetImage('bathroom', 'img/bathroom.jpg', 0, 0);
+var boots = new widgetImage('boots', 'img/boots.jpg ', 0, 0);
+var breakfast = new widgetImage('breakfast', 'img/breakfast.jpg', 0, 0);
+var bubblegum = new widgetImage('bubblegum', 'img/bubblegum.jpg ', 0, 0);
+var chair = new widgetImage('chair', 'img/chair.jpg', 0, 0);
+var cthulhhu = new widgetImage('cthulhhu', 'img/cthulhhu.jpg', 0, 0);
+var dogDuck = new widgetImage('dog-duck', 'img/dog-duck.jpg ', 0, 0);
+var dragon = new widgetImage('dragon', 'img/dragon.jpg', 0, 0);
+var pen = new widgetImage('pen', 'img/pen.jpg', 0, 0);
+var petSweep = new widgetImage('pet-sweep', 'img/pet-sweep.jpg', 0, 0);
+var scissors = new widgetImage('scissors', 'img/scissors.jpg', 0, 0);
+var shark = new widgetImage('shark', 'img/shark.jpg', 0, 0);
+var sweep = new widgetImage('sweep', 'img/sweep.png', 0, 0);
+var tauntaun = new widgetImage('tauntaun', 'img/tauntaun.jpg', 0, 0);
+var unicorn = new widgetImage('unicorn', 'img/unicorn.jpg', 0, 0);
+var usb = new widgetImage('usb', 'img/usb.gif', 0, 0);
+var waterCan = new widgetImage('water-can', 'img/water-can.jpg', 0, 0);
+var wineGlass = new widgetImage('wine-glass', 'img/wine-glass.jpg', 0, 0);
 
 // display 3 images on page via JS
 function renderWidgets() {
@@ -40,6 +41,23 @@ function renderWidgets() {
   var firstWidgetPic = document.createElement('img');
   firstWidgetPic.src = bag.filepath;
   firstWidgetSpot.appendChild(firstWidgetPic);
+  var firstWidgetSpot = document.getElementById('widget2');
+  var firstWidgetPic = document.createElement('img');
+  firstWidgetPic.src = bag.filepath;
+  firstWidgetSpot.appendChild(firstWidgetPic);
+  var firstWidgetSpot = document.getElementById('widget3');
+  var firstWidgetPic = document.createElement('img');
+  firstWidgetPic.src = bag.filepath;
+  firstWidgetSpot.appendChild(firstWidgetPic);
 }
 
 renderWidgets();
+
+function randomNum() {
+  return Math.floor(Math.random() * (allWidgets.length - 0) + 0);
+}
+
+// for loop that runs through the array of widgets and pulls back a number
+// for (var i = 0; i < allWidgets.length; i++) {
+//   array[i]
+// }
